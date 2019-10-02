1d22d563630e54869bf016c8a4cb6e1cbab1c66d
tegarsweefer <tegar.s@weefer.co.id>
2019-10-02 11:38:56 +0700

    input line on c sharp

---
+++ InputOutputCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputOutputCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan Nama: ");
+            var nama = Console.ReadLine();
+
+            Console.Write("Masukkan Alamat: ");
+            var alamat = Console.ReadLine();
+
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("Nama: "+nama);
+            Console.WriteLine("Alamat: "+alamat);
+        }
+    }
+}
