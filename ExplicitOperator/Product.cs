00a4c7e8be69d4b2dd2806b715b6099f6396470c
tegarsweefer <tegar.s@weefer.co.id>
2019-10-02 13:22:56 +0700

    explisit operator

---
+++ ExplicitOperator/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplicitOperator
+{
+    public class ProductView
+    {
+        public string name { set; get; } 
+        public string harga { set; get; } 
+        public string warna { set; get; }
+        public static explicit operator ProductView(Product obj)
+        {
+            ProductView dto = new ProductView();
+            dto.name = obj.name;
+            dto.harga = "Rp. " + obj.harga;
+            dto.warna = obj.warna;
+            return dto;
+        }
+    }
+    public class Product
+    {
+        public string name { set; get; }
+        public string harga { set; get; }
+        public string warna { set; get; }
+    }
+}
---
+++ ExplicitOperator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplicitOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            product.name = "Sepatu";
+            product.harga = "80.000";
+            product.warna = "Abu Abu";
+            ProductView productView= (ProductView)product;
+
+        }
+    }
+}
