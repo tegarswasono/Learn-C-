5c053b7bf03468d9f92ab8d257556a8fa49c3ea6
tegarsweefer <tegar.s@weefer.co.id>
2019-10-23 14:55:40 +0700

    Extension Method

---
+++ LearnCSharp/ExtensionMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int price = 8000;
+            Console.WriteLine(Utils.addRp(price));
+
+            List<int> queue = new List<int>();
+            queue.Add(4);
+            queue.Add(7);
+            queue.Add(14);
+            var result = queue.makeToBeString();
+            Console.WriteLine(result);
+
+            string number = "5";
+            long? resultLong = number.changeStringToBeLong();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LearnCSharp/ExtensionMethod/Utils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethod
+{
+    public static class Utils
+    {
+        public static string addRp(int price)
+        {
+            return "Rp. "+ price+",00";
+        }
+        public static string makeToBeString(this IEnumerable<int> queue)
+        {
+            string toBeString = "";
+            if (queue!=null)
+            {
+                foreach (int item in queue)
+                {
+                    toBeString += item+", ";
+                }
+            }
+            return toBeString;
+        }
+        //IComparable
+        public static long? changeStringToBeLong(this string number)
+        {
+            if (number==null || number=="")
+            {
+                return null;
+            }
+            return Convert.ToInt64(number);
+        }
+    }
+}
